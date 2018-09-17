52bd104a420622b257f8f61f099b2c5ead68689d
Jill Bender <jillcbender@gmail.com>
2018-09-16 22:19:50 -0700

    remove template from nav bar

--- linter/src/Components/NavBar.js
+++ linter/src/Components/NavBar.js
@@ -14,9 +14,6 @@ class NavBar extends Component {
                         <li className="nav-item active">
                             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/job-description-template">Job Description Template</a>
-                        </li>
                     </ul>
                 </div>
             </nav>
